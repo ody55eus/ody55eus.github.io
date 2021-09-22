2df16a5fd9043b52681a11ca24378c4fa6bda66c
Jonathan Pieper <ody55eus@mailbox.org>
2021-09-22 23:54:12 +0200

    Added Projects and some Literature notes.

--- init.el
+++ init.el
@@ -23,7 +23,44 @@
 (defun jp/init-webpage ()
     (setq org-roam-directory (concat
                               (locate-dominating-file buffer-file-name "INFO")
-                              "source/")))
+                              "source/")
+          org-roam-capture-templates '(("d" "default" plain
+         "%?\n\nSee also %a.\n"
+         :if-new (file+head
+                  "%<%Y%m%d%H%M%S>-${slug}.org"
+                  "#+title: ${title}\n")
+         :unnarrowed t)
+        ("j" "Projects" plain
+         "%?"
+         :if-new (file+head
+                  "Projects/%<%Y%m%d%H%M%S>-${slug}.org"
+                  "#+title: ${title}\n")
+         :clock-in :clock-resume
+         :unnarrowed t
+         )
+        ("l" "Literature")
+        ("ll" "Literature Note" plain
+         "%?\n\nSee also %a.\n* Links\n- %x\n* Notes\n"
+         :if-new (file+head
+                  "Literature/%<%Y%m%d%H%M%S>-${slug}.org"
+                  "#+title: ${title}\n")
+         :unnarrowed t
+         )
+        ("lr" "Bibliography reference" plain
+         "#+ROAM_KEY: %^{citekey}\n#+PROPERTY: type %^{entry-type}\n#+FILETAGS: %^{keywords}\n#+AUTHOR: %^{author}\n%?"
+         :if-new (file+head
+                  "References/${citekey}.org"
+                  "#+title: ${title}\n")
+         :unnarrowed t
+         )
+        ("c" "Code" plain
+         "%?\n\nSee also %a.\n"
+         :if-new (file+head
+                  "Code/%<%Y%m%d%H%M%S>-${slug}.org"
+                  "#+title: ${title}\n#+date: %U")
+         :unnarrowed t
+         )
+        )))
 
 (provide 'init)
 ;;; init.el ends here
