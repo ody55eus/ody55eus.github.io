e97cdf3d712c832c3f810574eecf9c8ace588eae
Jonathan Pieper <ody55eus@mailbox.org>
2021-09-24 03:10:44 +0200

    Removed requirements

--- publish.el
+++ publish.el
@@ -20,9 +20,19 @@
 ;;
 ;;; Code:
 
+(require 'package)
+
+(package-initialize)
+(unless package-archive-contents
+  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+  (package-refresh-contents))
+
+(package-install 'org-roam)
+
 (require 'ox-publish)
 (require 'ox-html)
-(require 'htmlize)
 (require 'org-roam)
 (require 'find-lisp)
 
