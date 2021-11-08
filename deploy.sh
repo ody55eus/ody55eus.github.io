adfc113c82f655ef57d3514b0677144f6cc82a99
Jonathan Pieper <ody55eus@mailbox.org>
2021-11-08 12:06:13 +0100

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 commit=$(git rev-parse HEAD)
 git branch -u origin/gh-pages gh-pages
+git fetch origin gh-pages
+echo $(git branch -v)
 git checkout gh-pages
 cp -Rf public/* .
 rm -rf public
